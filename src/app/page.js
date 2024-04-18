94b543bfcb2b5b9194cea0e67f726fdb7f721a7a
WebRizen <webrizen@gmail.com>
2024-04-18 20:15:53 +0530

    Add MainServices component to Home page
    This commit adds the `MainServices` component to the `Home` page in the app. The new component is added after the `Testimonial` component and before the `Services` component.

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import Action from "@/components/system/sections/Action";
 import Faq from "@/components/system/sections/Faq";
 import Features from "@/components/system/sections/Features";
 import Hero from "@/components/system/sections/Hero";
+import MainServices from "@/components/system/sections/MainServices";
 import Services from "@/components/system/sections/Services";
 import Testimonial from "@/components/system/sections/Testimonial";
 
@@ -11,6 +12,7 @@ export default function Home() {
       <Hero />
       <Features />
       <Testimonial />
+      <MainServices />
       <Services />
       <Faq />
       <Action />
